0a76d30fb88512152788f73559b33508f994658a
ZacharyJGaston <zgneworleans@aol.com>
2021-03-29 02:04:06 -0500

    Workshop 5 complete

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -5,20 +5,22 @@ import {
   Button,
   Label,
   Col,
-  Row
+  Row,
 } from "reactstrap";
 import { Link } from "react-router-dom";
-import { Control, Form, Errors, actions } from 'react-redux-form';
+import { Control, Form, Errors } from "react-redux-form";
 
-const required = val => val && val.length;
-const maxLength = len => val => !val || (val.length <= len);
-const minLength = len => val => val && (val.length >= len);
-const isNumber = val => !isNaN(+val);
-const validEmail = val => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length >= len;
+const isNumber = (val) => !isNaN(+val);
+const validEmail = (val) =>
+  /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
 
 class Contact extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       firstName: "",
       lastName: "",
@@ -34,18 +36,16 @@ class Contact extends Component {
         email: false,
       },
     };
-    
+
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(values) {
-    console.log("Current state is: " + JSON.stringify(values));
-    alert("Current state is: " + JSON.stringify(values));
+    this.props.postFeedback(values);
     this.props.resetFeedbackForm();
   }
 
   render() {
-
     return (
       <div className="container">
         <div className="row">
@@ -86,20 +86,24 @@ class Contact extends Component {
             </a>
           </div>
         </div>
+
         <div className="row row-content">
           <div className="col-12">
             <h2>Send us your Feedback</h2>
             <hr />
           </div>
           <div className="col-md-10">
-            <Form model = "feedbackForm" onSubmit={values => this.handleSubmit(values)}>
-              <Row className = "form-group">
+            <Form
+              model="feedbackForm"
+              onSubmit={(values) => this.handleSubmit(values)}
+            >
+              <Row className="form-group">
                 <Label htmlFor="firstName" md={2}>
                   First Name
                 </Label>
                 <Col md={10}>
                   <Control.text
-                    model=".firstName" 
+                    model=".firstName"
                     id="firstName"
                     name="firstName"
                     placeholder="First Name"
@@ -107,29 +111,29 @@ class Contact extends Component {
                     validators={{
                       required,
                       minLength: minLength(2),
-                      maxLength:maxLength(15)
+                      maxLength: maxLength(15),
                     }}
                   />
                   <Errors
                     className="text-danger"
                     model=".firstName"
                     show="touched"
-                    component= "div"
+                    component="div"
                     messages={{
-                      required: 'Required',
-                      minLength: 'Must be at least 2 characters',
-                      maxLength: 'Must be 15 characters or less'
+                      required: "Required",
+                      minLength: "Must be at least 2 characters",
+                      maxLength: "Must be 15 characters or less",
                     }}
                   />
                 </Col>
               </Row>
-              <Row className = "form-group">
+              <Row className="form-group">
                 <Label htmlFor="lastName" md={2}>
                   Last Name
                 </Label>
                 <Col md={10}>
                   <Control.text
-                  model=".lastName" 
+                    model=".lastName"
                     id="lastName"
                     name="lastName"
                     placeholder="Last Name"
@@ -137,29 +141,29 @@ class Contact extends Component {
                     validators={{
                       required,
                       minLength: minLength(2),
-                      maxLength:maxLength(15)
+                      maxLength: maxLength(15),
                     }}
                   />
                   <Errors
                     className="text-danger"
                     model=".lastName"
                     show="touched"
-                    component= "div"
+                    component="div"
                     messages={{
-                      required: 'Required',
-                      minLength: 'Must be at least 2 characters',
-                      maxLength: 'Must be 15 characters or less'
+                      required: "Required",
+                      minLength: "Must be at least 2 characters",
+                      maxLength: "Must be 15 characters or less",
                     }}
                   />
                 </Col>
               </Row>
-              <Row className = "form-group">
+              <Row className="form-group">
                 <Label htmlFor="phoneNum" md={2}>
                   Phone
                 </Label>
                 <Col md={10}>
                   <Control.text
-                    model=".phoneNum" 
+                    model=".phoneNum"
                     id="phoneNum"
                     name="phoneNum"
                     placeholder="Phone number"
@@ -167,58 +171,58 @@ class Contact extends Component {
                     validators={{
                       required,
                       minLength: minLength(10),
-                      maxLength:maxLength(15),
-                      isNumber
+                      maxLength: maxLength(15),
+                      isNumber,
                     }}
                   />
                   <Errors
                     className="text-danger"
                     model=".phoneNum"
                     show="touched"
-                    component= "div"
+                    component="div"
                     messages={{
-                      required: 'Required',
-                      minLength: 'Must be at least 10 numbers',
-                      maxLength: 'Must be 15 numbers or less',
-                      isNumber: 'Must be a number'
+                      required: "Required",
+                      minLength: "Must be at least 10 numbers",
+                      maxLength: "Must be 15 numbers or less",
+                      isNumber: "Must be a number",
                     }}
                   />
                 </Col>
               </Row>
-              <Row className = "form-group">
+              <Row className="form-group">
                 <Label htmlFor="email" md={2}>
                   Email
                 </Label>
                 <Col md={10}>
                   <Control.text
-                    model=".email" 
+                    model=".email"
                     id="email"
                     name="email"
                     placeholder="Email"
                     className="form-control"
                     validators={{
                       required,
-                      validEmail
+                      validEmail,
                     }}
                   />
                   <Errors
                     className="text-danger"
                     model=".email"
                     show="touched"
-                    component= "div"
+                    component="div"
                     messages={{
-                      required: 'Required',
-                      validEmail:'Invalid email address'
+                      required: "Required",
+                      validEmail: "Invalid email address",
                     }}
                   />
                 </Col>
               </Row>
-              <Row className = "form-group">
+              <Row className="form-group">
                 <Col md={{ size: 4, offset: 2 }}>
                   <div className="form-check">
                     <Label check>
                       <Control.checkbox
-                        model=".agree" 
+                        model=".agree"
                         name="agree"
                         className="form-check-input"
                       />{" "}
@@ -227,7 +231,7 @@ class Contact extends Component {
                   </div>
                 </Col>
                 <Col md={4}>
-                  <Control.select 
+                  <Control.select
                     model=".contactType"
                     name="contactType"
                     className="form-control"
@@ -237,13 +241,13 @@ class Contact extends Component {
                   </Control.select>
                 </Col>
               </Row>
-              <Row className = "form-group">
+              <Row className="form-group">
                 <Label htmlFor="feedback" md={2}>
                   Your Feedback
                 </Label>
                 <Col md={10}>
                   <Control.textarea
-                    model=".feedback" 
+                    model=".feedback"
                     id="feedback"
                     name="feedback"
                     rows="12"
@@ -251,7 +255,7 @@ class Contact extends Component {
                   />
                 </Col>
               </Row>
-              <Row className = "form-group">
+              <Row className="form-group">
                 <Col md={{ size: 10, offset: 2 }}>
                   <Button type="submit" color="primary">
                     Send Feedback
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -7,51 +7,71 @@ import Home from "./HomeComponent";
 import Contact from "./ContactComponent";
 import About from "./AboutComponent";
 import { Switch, Route, Redirect, withRouter } from "react-router-dom";
-import { connect } from 'react-redux';
-import { actions } from 'react-redux-form';
-import { postComment, fetchCampsites, fetchComments, fetchPromotions } from '../redux/ActionCreators';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { connect } from "react-redux";
+import { actions } from "react-redux-form";
+import {
+  postComment,
+  postFeedback,
+  fetchCampsites,
+  fetchComments,
+  fetchPromotions,
+  fetchPartners,
+} from "../redux/ActionCreators";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
-      campsites: state.campsites,
-      comments: state.comments,
-      partners: state.partners,
-      promotions: state.promotions
+    campsites: state.campsites,
+    comments: state.comments,
+    partners: state.partners,
+    promotions: state.promotions,
   };
 };
 
 const mapDispatchToProps = {
-  postComment: (campsiteId, rating, author, text) => (postComment(campsiteId, rating, author, text)),
-  fetchCampsites: () => (fetchCampsites()),
-  resetFeedbackForm: () => (actions.reset('feedbackForm')),
-  fetchComments: () => (fetchComments()),
-  fetchPromotions: () => (fetchPromotions())
+  postComment: (campsiteId, rating, author, text) =>
+    postComment(campsiteId, rating, author, text),
+  postFeedback: (feedback) => postFeedback(feedback),
+  fetchCampsites: () => fetchCampsites(),
+  resetFeedbackForm: () => actions.reset("feedbackForm"),
+  fetchComments: () => fetchComments(),
+  fetchPromotions: () => fetchPromotions(),
+  fetchPartners: () => fetchPartners(),
 };
-class Main extends Component {
 
+class Main extends Component {
   componentDidMount() {
     this.props.fetchCampsites();
     this.props.fetchComments();
     this.props.fetchPromotions();
-}
-
+    this.props.fetchPartners();
+  }
 
   render() {
     const HomePage = () => {
       return (
         <Home
           campsite={
-            this.props.campsites.campsites.filter((campsite) => campsite.featured)[0]
+            this.props.campsites.campsites.filter(
+              (campsite) => campsite.featured
+            )[0]
           }
           campsitesLoading={this.props.campsites.isLoading}
           campsitesErrMess={this.props.campsites.errMess}
           promotion={
-            this.props.promotions.promotions.filter((promotion) => promotion.featured)[0]
+            this.props.promotions.promotions.filter(
+              (promotion) => promotion.featured
+            )[0]
           }
           promotionLoading={this.props.promotions.isLoading}
           promotionErrMess={this.props.promotions.errMess}
-          partner={this.props.partners.filter((partner) => partner.featured)[0]}
+          partner={
+            this.props.partners.partners.filter(
+              (partner) => partner.featured
+            )[0]
+          }
+          partnerLoading={this.props.partners.isLoading}
+          partnerErrMess={this.props.partners.errMess}
         />
       );
     };
@@ -68,7 +88,7 @@ class Main extends Component {
           errMess={this.props.campsites.errMess}
           comments={this.props.comments.comments.filter(
             (comment) => comment.campsiteId === +match.params.campsiteId
-          )} 
+          )}
           commentsErrMess={this.props.comments.errMess}
           postComment={this.props.postComment}
         />
@@ -78,22 +98,43 @@ class Main extends Component {
     return (
       <div>
         <Header />
-                <TransitionGroup>
-                    <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
-                        <Switch>
-                            <Route path='/home' component={HomePage} />
-                            <Route exact path='/directory' render={() => <Directory campsites={this.props.campsites} />} />
-                            <Route path='/directory/:campsiteId' component={CampsiteWithId} />
-                            <Route exact path='/contactus' render={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} /> } />
-                            <Route exact path='/aboutus' render={() => <About partners={this.props.partners} /> } />
-                            <Redirect to='/home' />
-                        </Switch>
-                    </CSSTransition>
-                  </TransitionGroup>
-                <Footer />               
-            </div>
-        );
-    }
+        <TransitionGroup>
+          <CSSTransition
+            key={this.props.location.key}
+            classNames="page"
+            timeout={300}
+          >
+            <Switch>
+              <Route path="/home" component={HomePage} />
+              <Route
+                exact
+                path="/directory"
+                render={() => <Directory campsites={this.props.campsites} />}
+              />
+              <Route path="/directory/:campsiteId" component={CampsiteWithId} />
+              <Route
+                exact
+                path="/contactus"
+                render={() => (
+                  <Contact
+                    postFeedback={this.props.postFeedback}
+                    resetFeedbackForm={this.props.resetFeedbackForm}
+                  />
+                )}
+              />
+              <Route
+                exact
+                path="/aboutus"
+                render={() => <About partners={this.props.partners} />}
+              />
+              <Redirect to="/home" />
+            </Switch>
+          </CSSTransition>
+        </TransitionGroup>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
